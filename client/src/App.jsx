69998fa0041d19c91ff69df68fa1a2cb3cde33cb
Brodie M <brodiebowenmorgan76@gmail.com>
2025-08-05 15:30:20 +1000

    Testing page routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,26 @@
 import axios from "axios";
 import { useEffect } from "react";
-import './App.css'
-import { Routes, Route } from "react-router-dom";
-import { useState } from 'react'
-
-function Home() {
-  return <h1 className="text-2xl font-bold">Home Page</h1>;
-}
+import './App.css';
+import { Routes, Route } from "react-router-dom";;
+import { useState } from 'react';
+import Dashboard from "./pages/Dashboard";
+import About from "./pages/About"
 
 function App() {
+
   useEffect(() => {
     axios.get("http://localhost:5000/api/test").then(res => console.log(res.data)).catch(err => console.error(err));
-    
     axios.get('http://localhost:5000/api/env').then(res => console.log(res.data)).catch(err => console.error(err));
   }, []);
 
   return (
     <div className="p-6">
-      <h1 className="text-4xl font-bold text-blue-600 mb-4">
-        Tailwind is working!
-      </h1>
-      <p className="text-lg text-gray-700">
-        Check your browser console for API test responses.
-      </p>
+      <nav>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/About" element={<About />} />
+        </Routes>
+      </nav>
     </div>
   );
 }
---
+++ client/src/pages/Dashboard.jsx
@@ -0,0 +1,10 @@
+function Dashboard() {
+  return (
+    <div className="p-4">
+      <h1 className="text-3xl font-bold text-blue-500">Dashboard</h1>
+      <p>This is the Dashboard</p>
+    </div>
+  );
+}
+
+export default Dashboard;
